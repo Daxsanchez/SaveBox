1637b59b24b5f1c08a180ff72e2ee7d664f5442c
Daxsanchez <dax.saanchez@gmail.com>
2024-09-07 10:19:13 -0700

    Prueba

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,5 +11,6 @@ public class Main {
     public static void main(String[] args) {
         new VInicio().setVisible(true);
         System.out.println("Prueba");
+        System.out.println("Cmbio para que lo vea Alda");
     }
 }
