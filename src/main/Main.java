2301268c454f5d17810c46d151be400e5c89676a
Daxsanchez <dax.saanchez@gmail.com>
2024-10-20 21:13:35 -0700

    cambios main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import bd.BaseConexion;
 import controladores.CSocio;
 import java.util.ArrayList;
 import modelos.Socios;
@@ -14,9 +13,8 @@ import ventanas.Login;
 public class Main {
 
     public static void main(String[] args) {
-        Login dialog = new Login(new javax.swing.JFrame(), true);
-
-        /*dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+        /*Login dialog = new Login(new javax.swing.JFrame(), true);
+        dialog.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent e) {
                 System.exit(0);
